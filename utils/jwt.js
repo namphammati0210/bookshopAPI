28bf0763a07874472f0518ed5773003058b131e2
nampt <nampt@Phams-MacBook-Pro.local>
2022-08-01 14:17:57 +0700

    feat: 🎸 update jwt file

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 
-const createToken = async (payload) => {
+const createToken = (payload) => {
   try {
-    const token = await jwt.sign(payload, process.env.SECRET_KEY);
+    const token = jwt.sign(payload, process.env.SECRET_KEY);
     return token;
   } catch (error) {
     console.log("🚀 ~ file: jwt.js ~ line 7 ~ createToken ~ error", error);
